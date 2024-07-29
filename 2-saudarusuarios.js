ccb3946adfff794d3a7e3c47236b74c08b7ca321
Igor Miotto <igorenrique8810@gmail.com>
2024-07-29 16:00:16 -0300

    Update 2-saudarusuarios.js

--- 2-saudarusuarios.js
+++ 2-saudarusuarios.js
@@ -3,7 +3,10 @@ Crie uma função chamada greetUsers que aceita um array de nomes e
 exibe uma saudação para cada nome no console.
 
 Programa desenvolvido por:
+  - Gabriel
+ - Igor
  - Nataly
+ - Guilherme
 */
 
 function greetUsers(nomes) {
